b193438d9113d61f7b5b5f0532486541463bde2a
vipraiz <vipraizhas@gmail.com>
2021-07-07 21:09:32 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        verticalSwiping: true,
+        dots: true,
+        dotsClass: 'header-dots',
+        autoplay: 3000,
+    });
+});
